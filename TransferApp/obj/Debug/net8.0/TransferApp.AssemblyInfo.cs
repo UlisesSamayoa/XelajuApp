1222ca6fb09007d71a8001678e8740617c133b68
UlisesSamayoa <73310567+UlisesSamayoa@users.noreply.github.com>
2026-06-10 22:58:16 -0600

    Se agrega logica de filtro de tipos de documentos para modulo clients

--- TransferApp/obj/Debug/net8.0/TransferApp.AssemblyInfo.cs
+++ TransferApp/obj/Debug/net8.0/TransferApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944de30aecd32c9b4eca3b104540fc79d3c7040c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7397022206ea823c76b35b7701a33e923e34dac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
