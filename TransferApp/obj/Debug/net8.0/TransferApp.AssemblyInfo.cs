3acb7ea5e0df8d9ac17446399b44d8b95fd15bdb
UlisesSamayoa <73310567+UlisesSamayoa@users.noreply.github.com>
2026-07-12 22:51:04 -0600

    Se deja modal de reporte de comisiones, no funcionamiento

--- TransferApp/obj/Debug/net8.0/TransferApp.AssemblyInfo.cs
+++ TransferApp/obj/Debug/net8.0/TransferApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0ec532fd28a43a7ee291013d1b5973d999506b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552c557a8f58e7afe405c6240c25893dc8756534")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
