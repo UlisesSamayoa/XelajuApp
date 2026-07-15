c5b7ca4253cbe3ab82db4a4aa3d8875bf43a00d0
UlisesSamayoa <73310567+UlisesSamayoa@users.noreply.github.com>
2026-07-14 22:18:47 -0600

    Reporte de clientes funcinando

--- TransferApp/obj/Debug/net8.0/TransferApp.AssemblyInfo.cs
+++ TransferApp/obj/Debug/net8.0/TransferApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673892fda664b07523ed8d0eaef94de6b153d8b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea0eb979667b289ff06212f4225320765aeb858")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransferApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
